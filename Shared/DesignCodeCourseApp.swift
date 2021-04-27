4537a7effb481a777ca0b13ade3f8120854c5727
Rezuan Kassim <rezuankassim@hotmail.com>
2021-04-27 20:59:16 +0800

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Rezuan Kassim on 27/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
